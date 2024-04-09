61e6c97a8b382f326b0a21e2af735ebf51d0b1a5
HaoYuuu <x0958713773@gmail.com>
2024-04-09 15:39:41 +0800

    Update game3.py

--- cmds/game3.py
+++ cmds/game3.py
@@ -54,5 +54,6 @@ class Game3(Cog_Extension):
                 await ctx.send("**感謝遊玩**:video_game:\n**期待下次相遇**:wave:")
                 break
 
+
 async def setup(bot):
     await bot.add_cog(Game3(bot))
