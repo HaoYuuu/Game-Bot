6e1735fba9e0628ec664c33217312a394980524e
HaoYuuu <x0958713773@gmail.com>
2024-04-09 15:39:30 +0800

    Update bot.py

--- bot.py
+++ bot.py
@@ -11,33 +11,40 @@ intents = discord.Intents.all()
 
 bot = commands.Bot(command_prefix='.', intents=intents)
 
+
 @bot.event
 async def on_ready():
     print(">> Bot is online <<")
+
 
 @bot.command()
 async def load(ctx, extension):
     bot.load_extension(f"cmds.{extension}")
     await ctx.send(f"Loaded <{extension}> done.")
 
+
 @bot.command()
 async def unload(ctx, extension):
     bot.unload_extension(f"cmds.{extension}")
     await ctx.send(f"Un - Loaded <{extension}> done.")
+
 
 @bot.command()
 async def reload(ctx, extension):
     bot.reload_extension(f"cmds.{extension}")
     await ctx.send(f"Re - Loaded <{extension}> done.")
 
+
 async def load_extensions():
     for filename in os.listdir("./cmds"):
         if filename.endswith(".py"):
             await bot.load_extension(f"cmds.{filename[:-3]}")
 
+
 async def main():
     await load_extensions()
     await bot.start(jdata["TOKEN"])
+
 
 if __name__ == "__main__":
     asyncio.run(main())
